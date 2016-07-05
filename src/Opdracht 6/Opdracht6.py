30d4c7b4e4eb0a902c45db5739853ffb70b03413
Joshua <joshua@theinternets.nl>
2016-07-05 10:59:05 +0200

    import old opdracht 6

--- src/Opdracht 6/Opdracht6.py
+++ src/Opdracht 6/Opdracht6.py
@@ -1,13 +1,38 @@
-import argparse
+from Crypto.Cipher import AES
+import binascii
 
-parser = argparse.ArgumentParser(description='Dit is Opdqdracht 6, IFSCP 1516')
-parser.parse_args()
+Opdracht = 6
+key = 'mysecretpassword'
+plaintext = 'Secret Message A'
+ciphertext = binascii.unhexlify('e8da47acc08bc751745ef8fbff44e107')
 
-args = parser.parse_args()
+def welcome():
+    print("Welkom bij opdracht " + str(Opdracht))
+    key = raw_input("Wat is de sleutel? (16 of 32 karakters)")
+    keuze = raw_input("Wat wil je doen, 1 voor coderen, 2 voor de decoderen: ")
+    try:
+        if(keuze == "1"):
+            plain = raw_input("Wat is de plaintext? (16 of 32 karakters)")
+            print(encrypt(plain, key))
+        elif(keuze == "2"):
+            cipher = raw_input("Wat is de cipthertext?")
+            print(decrypt(cipher, key))
+        else:
+            print("Geen goede keuze :(")
+    except:
+        print("Er ging iets fout. Waarschijnlijk heeft dit te maken met de ingevoerde tekst")
 
-class Opdracht6:
-    def __init__(self):
-        message = ""
 
-if __name__ == "__main__":
-    Opdracht6()
+def encrypt(plain, key):
+    encobj = AES.new(key, AES.MODE_ECB)
+    ciphertext = encobj.encrypt(plain)
+    # Resulting ciphertext in hex
+    return ciphertext.encode('hex')
+
+def decrypt(ciphertext, key):
+    decobj = AES.new(key, AES.MODE_ECB)
+    plaintext = decobj.decrypt(ciphertext.decode('hex'))
+    # Resulting plaintext
+    return plaintext
+
+welcome()
